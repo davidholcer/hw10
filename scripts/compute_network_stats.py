1c8ca1a57ef28b2f0ea5055a57a0f9e5b1992113
David Holcer <davidholcer@gmail.com>
2023-12-04 19:45:01 -0500

    finalized

--- scripts/compute_network_stats.py
+++ scripts/compute_network_stats.py
@@ -2,20 +2,21 @@ import argparse
 import json
 import networkx as nx
 
+#here it's loading the interaction network
 def load_interaction_network(input_file):
     with open(input_file, 'r', encoding='utf-8') as jsonfile:
         interaction_network = json.load(jsonfile)
     return interaction_network
 
-def compute_stats(interaction_network):
-    # Create an undirected graph from the interaction network
-    G = nx.Graph(interaction_network)
+#computing the stats
+def compute_stats(graph):
+    # Compute weighted degree centrality
+    weighted_degree_centrality = dict(graph.degree(weight='weight'))
 
-    # Compute centrality measures
-    degree_centrality = nx.degree_centrality(G)
-    weighted_degree_centrality = nx.degree_centrality(G, weight='weight')
-    closeness_centrality = nx.closeness_centrality(G)
-    betweenness_centrality = nx.betweenness_centrality(G)
+    # Compute other centrality measures
+    degree_centrality = nx.degree_centrality(graph)
+    closeness_centrality = nx.closeness_centrality(graph, distance='weight')
+    betweenness_centrality = nx.betweenness_centrality(graph, weight='weight')
 
     # Get the top three characters for each centrality measure
     top_degree = sorted(degree_centrality, key=degree_centrality.get, reverse=True)[:3]
@@ -35,8 +36,8 @@ def compute_stats(interaction_network):
 
 def save_stats(output_file, stats):
     # Write the stats to a JSON file
-    with open(output_file, 'w', encoding='utf-8') as jsonfile:
-        json.dump(stats, jsonfile, ensure_ascii=False, indent=2)
+    with open(output_file, 'w', encoding='utf-8') as theOutput:
+        json.dump(stats, theOutput, indent=2)
 
 if __name__ == "__main__":
     # Command-line arguments
@@ -48,8 +49,17 @@ if __name__ == "__main__":
     # Load the interaction network from the input file
     interaction_network = load_interaction_network(args.input)
 
+    # Create a weighted directed graph from the interaction network
+    # it is directed since both a and b are accounted for but in reality 
+    # functions as undirected as a->b iff b->a
+    G = nx.DiGraph()
+
+    for source, targets in interaction_network.items():
+        for target, weight in targets.items():
+            G.add_edge(source, target, weight=weight)
+
     # Compute network statistics
-    stats = compute_stats(interaction_network)
+    stats = compute_stats(G)
 
     # Save the stats to the output file
     save_stats(args.output, stats)
